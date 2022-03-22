3dddb5f0e44bc3a7e3adaa2c05a9eef657086b93
Nando <nandodavila94@gmail.com>
2022-03-22 09:19:39 -0400

    end of section 3

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,21 +1,35 @@
-import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react"
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme, CssBaseline } from "@mui/material";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/products"
 import Header from "./Header";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode)
+  }
   
   
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <Header/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
         <Catalog/>
       </Container>
       
-    </>
+    </ThemeProvider>
   );
 }
 
