6de1443b3c10a1ea4719ecbd61abf1f26f78f5ff
fernandoDavila <fdavila@buildtechnologygroup.com>
2022-03-29 15:59:49 -0400

    end of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { ThemeProvider } from "@emotion/react";
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 function App() {
@@ -26,7 +31,12 @@ function App() {
       <CssBaseline/>
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog/>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/catalog' component={Catalog}/>
+        <Route exact path='/catalog/:id' component={ProductDetails}/>
+        <Route exact path='/about' component={AboutPage}/>
+        <Route exact path='/contact' component={ContactPage}/>
+
       </Container>
       
     </ThemeProvider>
